45b1996b355887d84608d2abbf25d3de0080cbe1
Yamato-TTGL <yamato2465@outlook.com>
2026-04-03 12:41:21 -0500

    feat/add_models
    models generados a partir de las indicaciones dadas por el docente Octavio

---
+++ odoo/addons/actividades_complementarias/models/sii_tipousuario.py
@@ -0,0 +1,4 @@
+from odoo import models, fields
+class SiiTipoUsuario(models.Model):
+    _name='sii.tipousuario'; _description='SII — TipoUsuario'; _rec_name='nombre'
+    nombre=fields.Char(string='Nombre',required=True)
