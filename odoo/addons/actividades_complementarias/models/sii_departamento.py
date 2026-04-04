fafda800cce9fcaddd99cd7e715c1932d28ced73
Yamato-TTGL <yamato2465@outlook.com>
2026-04-03 23:20:10 -0500

    feat/correcciones
    correcciones pull reques anterior

--- odoo/addons/actividades_complementarias/models/sii_departamento.py
+++ odoo/addons/actividades_complementarias/models/sii_departamento.py
@@ -1,4 +1,12 @@
 from odoo import models, fields
+
+
 class SiiDepartamento(models.Model):
-    _name='sii.departamento'; _description='SII — catalogoDepartamentos'; _rec_name='nombre_departamento'
-    nombre_departamento=fields.Char(string='nombreDepartamento',required=True)
+    _name = 'sii.departamento'
+    _description = 'SII — catalogoDepartamentos'
+    _rec_name = 'nombre_departamento'
+
+    nombre_departamento = fields.Char(
+        string='nombreDepartamento',
+        required=True
+    )
