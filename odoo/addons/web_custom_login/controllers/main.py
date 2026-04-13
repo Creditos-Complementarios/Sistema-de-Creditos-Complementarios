9e1e40a08429de1ea075cb3f10ce2e79ddc4eff9
EliPg <eliderriverasolis23@gmail.com>
2026-04-13 05:52:59 -0500

    fix/Correción de errores de sintaxis

--- odoo/addons/web_custom_login/controllers/main.py
+++ odoo/addons/web_custom_login/controllers/main.py
@@ -1,6 +1,7 @@
 from odoo.addons.web.controllers.home import Home
 from odoo import http
 
+
 class CustomHome(Home):
     @http.route('/web/login', type='http', auth='none', sitemap=False)
     def web_login(self, redirect=None, **kw):
