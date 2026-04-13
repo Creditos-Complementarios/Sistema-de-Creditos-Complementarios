96e3774a7563c403d5b5ffacf06d47e547fd4410
EliPg <eliderriverasolis23@gmail.com>
2026-04-12 23:34:30 -0500

    fix/Reparación de error de Indexación

--- odoo/addons/actividades_complementarias/wizards/wizard_aprobar_propuesta.py
+++ odoo/addons/actividades_complementarias/wizards/wizard_aprobar_propuesta.py
@@ -32,7 +32,7 @@ class WizardAprobarPropuesta(models.TransientModel):
             raise ValidationError('Debe asignar los creditos a la actividad.')
         # bypass_edit_protection: el Comité asigna créditos como acción de negocio
         self.propuesta_id.actividad_id.sudo().with_context(bypass_edit_protection=True).write(
-    {'creditos': self.creditos}
+        {'creditos': self.creditos}
         )
         self.propuesta_id.action_aprobar()
         return {'type': 'ir.actions.act_window_close'}
